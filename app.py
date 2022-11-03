74b10a3739fb307d61842825fb561ad6691af33c
diogoheck <diogo.heck.dev@gmail.com>
2022-11-03 18:07:40 -0300

    Versao Inicial

---
+++ app.py
@@ -0,0 +1,45 @@
+# Ler planilha CSV
+# Criar um dicionario dos dados dos colaboradores
+# Trabalhar os dados 
+# Pegar codigo empresa
+# Classe tirar o numero
+# CPF - tirar pontos e traço
+# PIS - tirar pontos
+# trabalhar a data para digitar
+# tipo admissao , retirar o numero
+# pegar a funcao
+# pegar o tipo de colaborador
+# pegar e trabalhar o valor salario
+# Se tipo de contrato for prazo indeterminado nao ler prazo de contrato
+# senao ler prazo de contrato
+
+# logar no unico
+# entrar no unico->folha
+# ALT + A, D -> Admissao preliminar
+# digitar numero da empresa
+# enter
+# enter
+# digita o nome do colaborador
+# enter
+# digita classe
+# enter
+# digita cpf
+# digita pis
+# digita data admissao
+# digita data nascimento
+# digita tipo admissao
+# enter
+# enter
+# digita funcao
+# enter
+# digita tipo colaborador 
+# enter
+# digita salario base
+# enter
+# digita tipo de contrato
+# se tipo contrato for 3 entao:
+# enter
+# digita o prazo em dias
+# pagdown
+# se o prazo for indeterminado
+# pagdown
